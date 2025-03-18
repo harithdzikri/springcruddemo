2e6716f61803b9f32ba4d5f54dcf9dbbedc0ef94
harithdzikri <harith.hashim@bertelsmann.de>
2025-03-18 12:53:16 +0800

    simplify using Spring Data REST
    no need boiler plate code of creating CRUD method in the restcontroller class

--- cruddemo/src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeRepository.java
+++ cruddemo/src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeRepository.java
@@ -4,4 +4,5 @@ import com.luv2code.springboot.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+    
 }
